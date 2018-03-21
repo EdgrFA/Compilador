09e6b2d2609396a823ec45a8ee279128d0063b05
andgvo <andgvo@gmail.com>
2018-03-21 07:38:20 -0600

    FloatM

---
+++ src/Automatas/descensoRecursivo/FloatM.java
@@ -0,0 +1,41 @@
+package Automatas.descensoRecursivo;
+
+public class FloatM {
+    private float valor;
+    
+    public FloatM(float valor){
+        this.valor = valor; 
+    }
+    
+    public static float prod(FloatM factor1, FloatM factor2){
+        return factor1.getValor()*factor2.getValor();
+    }
+    
+    public static float div(FloatM numerador, FloatM divisor){
+        return numerador.getValor()/divisor.getValor();
+    }
+    
+    public void suma(FloatM sumando1, FloatM sumando2){
+        valor = sumando1.getValor()+sumando2.getValor();
+    }
+    
+    public void resta(FloatM minuendo, FloatM sustraendo){
+        valor = minuendo.getValor()+sustraendo.getValor();
+    }
+    
+    public void producto(FloatM factor1, FloatM factor2){
+        valor = factor1.getValor()*factor2.getValor();
+    }
+    
+    public void division(FloatM numerador, FloatM divisor){
+        valor = numerador.getValor()/divisor.getValor();
+    }
+    
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }    
+}
