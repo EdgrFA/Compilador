1e4fba39fa537f2887bd223d152eca87cfbe3109
SoldierSOAD <foae_97@hotmail.com>
2018-03-21 12:54:41 -0600

    Tokens

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -15,7 +15,14 @@ public class Calculadora {
         afns.crearAFN('*'); //3
         afns.crearAFN('/'); //4
         afns.crearAFN('('); //5
-        afns.crearAFN(')'); //6
+        afns.crearAFN(')'); //6     
+        atmSEN(afns); //7
+        atmCOS(afns); //8
+        atmTAN(afns); //9
+        afns.crearAFN('^'); //10
+        atmEXP(afns); //11
+        atmLN(afns); //12
+        atmLOG(afns); //13
         AFD afd = new AFD(afns);
         
         Tokens.NUM = afns.getTokenAFN(0);
@@ -25,21 +32,18 @@ public class Calculadora {
         Tokens.DIV = afns.getTokenAFN(4);
         Tokens.PAR_I = afns.getTokenAFN(5);
         Tokens.PAR_D = afns.getTokenAFN(6);
+        Tokens.SIN = afns.getTokenAFN(7);
+        Tokens.COS = afns.getTokenAFN(8);
+        Tokens.TAN = afns.getTokenAFN(9);
+        Tokens.POT = afns.getTokenAFN(10);
+        Tokens.EXP = afns.getTokenAFN(11);
+        Tokens.LN = afns.getTokenAFN(12);
+        Tokens.LOG = afns.getTokenAFN(13);
         
         Tokens.TokenInfo();
         afd.imprimirTablaTransiciones();
         
-        AnalizadorLexico al = new AnalizadorLexico("5.5+(4-2)", afd);
-        int token = al.obtenerToken();
-        System.out.println("Token: " + token + ", Lexema: " + al.getLexema());
-        al.regresarToken();
-        token = al.obtenerToken();
-        System.out.println("Token: " + token + ", Lexema: " + al.getLexema());
-        token = al.obtenerToken();
-        System.out.println("Token: " + token + ", Lexema: " + al.getLexema());
-        al.regresarToken();
-        token = al.obtenerToken();
-        System.out.println("Token: " + token + ", Lexema: " + al.getLexema());
+        AnalizadorLexico al = new AnalizadorLexico("5.5+(4-2)*LOG(e^5)", afd);
     }
 
     //NUM -> 0
@@ -53,4 +57,49 @@ public class Calculadora {
         afns.operacionSigno(1);
         afns.concatenar(0, 1);
     }
+
+    public static void atmSEN(AFNs afns){
+        afns.crearAFN('S'); //7
+        afns.crearAFN('I'); //8
+        afns.crearAFN('N'); //9
+        afns.concatenar(8, 9);
+        afns.concatenar(7, 8);
+    }
+    
+    public static void atmCOS(AFNs afns){
+        afns.crearAFN('C'); //8
+        afns.crearAFN('O'); //9
+        afns.crearAFN('S'); //10
+        afns.concatenar(9, 10);
+        afns.concatenar(8, 9);
+    }
+    
+    public static void atmTAN(AFNs afns){
+        afns.crearAFN('T'); //9
+        afns.crearAFN('A'); //10
+        afns.crearAFN('N'); //11
+        afns.concatenar(10, 11);
+        afns.concatenar(9, 10);
+    }
+    
+    public static void atmEXP(AFNs afns){
+        afns.crearAFN('e'); //11
+        afns.crearAFN('^'); //12
+        afns.concatenar(11, 12);
+    }
+    
+    public static void atmLN(AFNs afns){
+        afns.crearAFN('L'); //12
+        afns.crearAFN('N'); //13
+        afns.concatenar(12, 13);
+    }
+    
+    public static void atmLOG(AFNs afns){
+        afns.crearAFN('L'); //13
+        afns.crearAFN('O'); //14
+        afns.crearAFN('G'); //15
+        afns.concatenar(14, 15);
+        afns.concatenar(13, 14);
+    }
+    
 }
--- src/Calculadora/Tokens.java
+++ src/Calculadora/Tokens.java
@@ -17,6 +17,13 @@ public class Tokens {
     public static int PROD;
     public static int PAR_I;
     public static int PAR_D;
+    public static int SIN;
+    public static int COS;
+    public static int TAN;
+    public static int POT;
+    public static int EXP;
+    public static int LN;
+    public static int LOG;
 
     public static void TokenInfo() {
         System.out.println("Token NUM = " + Tokens.NUM);
@@ -26,6 +33,13 @@ public class Tokens {
         System.out.println("Token PROD = " + Tokens.PROD);
         System.out.println("Token PAR_I = " + Tokens.PAR_I);
         System.out.println("Token PAR_D = " + Tokens.PAR_D);
+        System.out.println("Token SIN = " + Tokens.SIN);
+        System.out.println("Token COS = " + Tokens.COS);
+        System.out.println("Token TAN = " + Tokens.TAN);
+        System.out.println("Token POT = " + Tokens.POT);
+        System.out.println("Token EXP = " + Tokens.EXP);
+        System.out.println("Token LN = " + Tokens.LN);
+        System.out.println("Token LOG = " + Tokens.LOG);
         System.out.println("");
     }
     
